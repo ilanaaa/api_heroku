9df42072996cb6713097970139bccdf7f999b3dc
Marina <marina@seekingalpha.com>
2014-10-28 17:16:18 +0200

    del root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  root 'application#parse_data'
-
   # Example of regular route:
   get 'ipad_ratings/get_data' => 'ipad_ratings#get_data'
   #   get 'products/:id' => 'catalog#view'
