35c122ae82d4184c356c09789e5621c8bea9205f
Marina <marina@seekingalpha.com>
2014-11-05 12:32:40 +0200

    added iphone model mod

--- db/migrate/20141105102323_create_iphone_ratings.rb
+++ db/migrate/20141105102323_create_iphone_ratings.rb
@@ -1,6 +1,13 @@
 class CreateIphoneRatings < ActiveRecord::Migration
   def change
     create_table :iphone_ratings do |t|
+  create_table :ipad_ratings do |t|
+      t.string :date 
+      t.integer :onestar 
+      t.integer :twostar 
+      t.integer :threestar 
+      t.integer :fourstar 
+      t.integer :fivestar 
 
       t.timestamps
     end
